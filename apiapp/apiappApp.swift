7ca6cf9003083f418f6702cc7779bb6554b54e89
Lucie <lucie.garofalo@etu.univ-savoie.fr>
2024-01-23 13:43:41 +0100

    Initial Commit

---
+++ apiapp/apiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiappApp.swift
+//  apiapp
+//
+//  Created by Lucie GAROFALO on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct apiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
